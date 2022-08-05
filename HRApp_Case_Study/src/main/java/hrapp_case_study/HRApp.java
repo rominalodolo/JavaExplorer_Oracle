6a09ea274d78aa144c006d13d9172faa550fb2da
Romina <lodoloromina@gmail.com>
2022-08-05 13:24:09 +0200

    update

--- HRApp_Case_Study/src/main/java/hrapp_case_study/HRApp.java
+++ HRApp_Case_Study/src/main/java/hrapp_case_study/HRApp.java
@@ -7,9 +7,24 @@ public class HRApp {
         Employee e1 = new Employee(937, "Romina", 27789);
         Employee e2 = new Employee(780, "Jackie", 69932);
 
-        System.out.println(e1);
-        System.out.println(e2);
 
+        Department dept = new Department("Education");
+
+        dept.addEmp(e1);
+        dept.addEmp(e2);
+        dept.addEmp(new Employee(429, "Jane", 62874));
+
+
+        Employee[] emps = dept.getEmployees();
+
+        for (Employee emp : emps) {
+            System.out.println("Emp " + emp);
+        }
+
+        System.out.println("Total " + dept.getTotalSalary());
+
+//        System.out.println(e1);
+//        System.out.println(e2);
     }
 }
 
