df745a433f40ccc7c24a6bcb0ab5d31a46ab75d4
Romina <lodoloromina@gmail.com>
2022-08-05 10:41:05 +0200

    update

---
+++ HRApp_Case_Study/src/main/java/hrapp_case_study/Employee.java
@@ -0,0 +1,31 @@
+package hrapp_case_study;
+
+public class Employee {
+    private int ID;
+    private String name;
+    private double salary;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ HRApp_Case_Study/src/main/java/hrapp_case_study/HRApp.java
@@ -0,0 +1,9 @@
+package hrapp_case_study;
+
+public class HRApp {
+    public static void main(String[]args){
+        System.out.println("HR App Starts");
+
+    }
+}
+
